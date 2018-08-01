c8e5fe158d85344f0e30287a6965b00391a94336
sysups <info@sysups.nl>
2018-08-01 16:56:35 +0200

    initial commit

---
+++ server1/provision-domain.ps1
@@ -0,0 +1,7 @@
+$joinCred = New-Object pscredential -ArgumentList ([pscustomobject]@{
+    UserName = $null
+    Password = (ConvertTo-SecureString -String 'TempJoinPA$$' -AsPlainText -Force)[0]
+})
+Add-Computer -Domain "lab.pri" -Options UnsecuredJoin,PasswordPass -Credential $joinCred
+
+Restart-Computer
